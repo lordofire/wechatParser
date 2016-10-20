9782441779a708d5840b4aafc41111b4f2195ebc
lordofire <wangjianan.zju@gmail.com>
2016-10-20 00:47:10 -0700

    Adding friends class and friends meta parsing from WCDB_Contact.sqlite   Still needs bug fixes
      Changes to be committed:
    	new file:   friends.py
    	modified:   util.py

---
+++ friends.py
@@ -0,0 +1,77 @@
+from util import *
+import sqlite3
+import hashlib
+
+"""
+Some global variables
+"""
+Friends = {}
+m = hashlib.md5()
+
+"""
+Wechat Friend class, containing most of the useful information of a wechat friend
+"""
+class Friend:
+    def __init__(self, userName, nickName, alias, contactRemark, country, state, city, whatsup):
+        self.userName = userName
+        m.update(userName.encode('utf-8'))
+        self.uNmd5 = m.hexdigest()
+        self.nickName = nickName
+        self.alias = alias
+        self.contactRemark = contactRemark
+        self.country = country
+        self.state = state
+        self.city = city
+        self.whatsup = whatsup
+
+    def __str__(self):
+        return "Friend %s with md5(%s), nickName:%s, alias:%s, contactRemark:%s, country:%s, state:%s, city:%s, whatsup:%s" % (self.userName, str(self.uNmd5), self.nickName, self.alias, self.contactRemark, self.country, self.state, self.city, self.whatsup)
+
+"""
+collectFriendsMeta(): parse/collect the friend meta from WCDB_Contact.sqlite
+"""
+def collectFriendsMeta():
+    conn = sqlite3.connect("/Users/jiananwa/Documents/wechat_backup_jianan/8cd7299222b76fd76e53cc56e424a363/DB/WCDB_Contact.sqlite")
+    c = conn.cursor()
+    c.execute("SELECT userName, dbContactRemark, dbContactProfile FROM Friend")
+    
+    for friend in c.fetchall():
+        print friend[0]
+        """ First parse the contact remark """
+        remarkDat = parsedbContactRemark(friend[1])
+        nickName = ''
+        if 0x0a in remarkDat.keys():
+            nickName = remarkDat[0x0a]
+        alias = ''
+        if 0x12 in remarkDat.keys():
+            alias = remarkDat[0x12]
+        contactRemark = ''
+        if 0x1a in remarkDat.keys():
+            contactRemark = remarkDat[0x1a]
+
+        """ Then parse the contact profile """
+        profileDat = parsedbContactProfile(friend[2])
+        country = ''
+        if(profileDat.__len__() > 0):
+            country = profileDat[0]
+        state = ''
+        if(profileDat.__len__() > 1):
+            state = profileDat[1]
+        city = ''
+        if(profileDat.__len__() > 2):
+            city = profileDat[2]
+        whatsup = ''
+        if(profileDat.__len__() > 3):            
+            whatsup = profileDat[3]
+        
+        Friends[friend[0]] = Friend(friend[0], nickName, alias, contactRemark, country, state, city, whatsup)
+
+    for userName, friend in Friends.iteritems():
+        print friend
+        
+
+def main():
+    collectFriendsMeta()
+
+if __name__ == "__main__":
+    main()
--- util.py
+++ util.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python
-
 """
 parsedbContactRemark: input blob buffer for dbContactRemark in WCDB_Contact.sqlite
 format: byte size key + size + value
 output: a map of key/value pairs
 """
-def parsedbContactRemark( binBuffer ):
+def parsedbContactRemark(binBuffer):
     binArray = bytearray(binBuffer)
     contactRemarkDict = {}
     index = 0
@@ -18,14 +16,14 @@ def parsedbContactRemark( binBuffer ):
             val = str(binArray[index : index+size])
         contactRemarkDict[key] = val
         index += size
-    return friendBinDict
+    return contactRemarkDict
 
 """
 parsedbContactRemark: input blob buffer for dbContactProfile in WCDB_Contact.sqlite
 format: size + value
 output: a array values
 """
-def parsedbContactProfile( binBuffer ):
+def parsedbContactProfile(binBuffer):
     binArray = bytearray(binBuffer)
     contactProfileArray = []
     index = 3
