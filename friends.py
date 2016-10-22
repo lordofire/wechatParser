604cdd25958f49c7b03f12af76d576b7e8185bec
lordofire <wangjianan.zju@gmail.com>
2016-10-22 00:44:22 -0700

    Add the profile photos/ chatroom parsing  Current algorithm has some issue when parsing chatroom blobs with   large number of members.
     Changes to be committed:
    	modified:   friends.py
    	modified:   util.py

--- friends.py
+++ friends.py
@@ -6,26 +6,48 @@ import hashlib
 Some global variables
 """
 Friends = {}
+ChatRooms = {}
 m = hashlib.md5()
 
 """
 Wechat Friend class, containing most of the useful information of a wechat friend
 """
 class Friend:
-    def __init__(self, userName, nickName, alias, contactRemark, country, state, city, whatsup):
+    def __init__(self, userName, nickName, alias, contactRemark, MiniPhoto, HDPhoto, country, state, city, whatsup):
         self.userName = userName
         m.update(userName.encode('utf-8'))
         self.uNmd5 = m.hexdigest()
         self.nickName = nickName
         self.alias = alias
         self.contactRemark = contactRemark
+        self.MiniPhoto = MiniPhoto
+        self.HDPhoto = HDPhoto
         self.country = country
         self.state = state
         self.city = city
         self.whatsup = whatsup
 
     def __str__(self):
-        return "Friend " + self.userName.encode('utf-8') + " with md5(" + self.uNmd5 +  "), nickName:" + self.nickName + ", alias:" + self.alias + ", contactRemark:" + self.contactRemark + ", country:" + self.country + ", state:" + self.state + ", city:" + self.city + ", whatsup:" + self.whatsup
+        return "Friend " + self.userName.encode('utf-8') + " with md5(" + self.uNmd5 +  "), nickName:" + self.nickName + ", alias:" + self.alias + ", contactRemark:" + self.contactRemark + ", MiniPhoto:" + self.MiniPhoto + ", HDPhoto:" + self.HDPhoto +", country:" + self.country + ", state:" + self.state + ", city:" + self.city + ", whatsup:" + self.whatsup
+
+"""
+Wechat ChatRoom class, containing most of the useful information of a wechat chatRoom
+"""
+class ChatRoom:
+    def __init__(self, roomName, memberList, roomCreator, roomDataXML):
+        self.roomName = roomName
+        m.update(roomName.encode('utf-8'))
+        self.uNmd5 = m.hexdigest()
+        self.memberList = memberList
+        self.roomCreator = roomCreator
+        self.roomDataXML = roomDataXML
+
+    def __str__(self):
+        membersList = ''
+        for member in self.memberList:
+            membersList += member + ", "
+
+        return "ChatRoom " + self.roomName.encode('utf-8') + " with md5(" + self.uNmd5 +  "), memberList:" + membersList + "roomCreator:" + self.roomCreator + ", roomDataXML:" + self.roomDataXML
 
 """
 collectFriendsMeta(): parse/collect the friend meta from WCDB_Contact.sqlite
@@ -33,46 +55,69 @@ collectFriendsMeta(): parse/collect the friend meta from WCDB_Contact.sqlite
 def collectFriendsMeta():
     conn = sqlite3.connect("/Users/jiananwa/Documents/wechat_backup_jianan/8cd7299222b76fd76e53cc56e424a363/DB/WCDB_Contact.sqlite")
     c = conn.cursor()
-    c.execute("SELECT userName, dbContactRemark, dbContactProfile FROM Friend")
+    c.execute("SELECT userName, dbContactRemark, dbContactHeadImage, dbContactProfile, dbContactChatRoom FROM Friend")
     
     for friend in c.fetchall():
-        print friend[0]
-        """ First parse the contact remark """
-        remarkDat = parsedbContactRemark(friend[1])
-        nickName = ''
-        if 0x0a in remarkDat.keys():
-            nickName = remarkDat[0x0a]
-        alias = ''
-        if 0x12 in remarkDat.keys():
-            alias = remarkDat[0x12]
-        contactRemark = ''
-        if 0x1a in remarkDat.keys():
-            contactRemark = remarkDat[0x1a]
+        if "@chatroom" not in friend[0]:
+            """ If the entry is a friend record """
+            """ First parse the contact remark """
+            remarkDat = parsedbContactRemark(friend[1])
+            nickName = ''
+            if 0x0a in remarkDat.keys():
+                nickName = remarkDat[0x0a]
+                alias = ''
+            if 0x12 in remarkDat.keys():
+                alias = remarkDat[0x12]
+                contactRemark = ''
+            if 0x1a in remarkDat.keys():
+                contactRemark = remarkDat[0x1a]
 
-        """ Then parse the contact profile """
-        profileDat = parsedbContactProfile(friend[2])
-        country = ''
-        if(profileDat.__len__() > 0):
-            country = profileDat[0]
-        state = ''
-        if(profileDat.__len__() > 1):
-            state = profileDat[1]
-        city = ''
-        if(profileDat.__len__() > 2):
-            city = profileDat[2]
-        whatsup = ''
-        if(profileDat.__len__() > 3):            
-            whatsup = profileDat[3]
-        
-        Friends[friend[0]] = Friend(friend[0], nickName, alias, contactRemark, country, state, city, whatsup)
+            """ Next parse the contact head Image """
+            (MiniPhoto, HDPhoto) = parsedbContactHeadImage(friend[2])
+
+            """ Then parse the contact profile """
+            profileDat = parsedbContactProfile(friend[3])
+            country = ''
+            if(profileDat.__len__() > 0):
+                country = profileDat[0]
+            state = ''
+            if(profileDat.__len__() > 1):
+                state = profileDat[1]
+            city = ''
+            if(profileDat.__len__() > 2):
+                city = profileDat[2]
+            whatsup = ''
+            if(profileDat.__len__() > 3):
+                whatsup = profileDat[3]
+
+            Friends[friend[0]] = Friend(friend[0], nickName, alias, contactRemark, MiniPhoto, HDPhoto, country, state, city, whatsup)
+        else:
+            """ If the entry is a chatroom record """
+            (memberList, roomCreator, RoomDataXML) = parsedbContactChatRoom(friend[4])
+
+            ChatRooms[friend[0]] = ChatRoom(friend[0], memberList, roomCreator, RoomDataXML)
 
 def main():
     """ Initial the Friends Meta """
     collectFriendsMeta()
 
+    print "==========================================="
+    print "Friends List"
+    print "==========================================="
+
     """ Iterate through the Friends list and print the content """
     for userName, friend in Friends.iteritems():
         print friend
+        print ''
+
+    print "==========================================="
+    print "ChatRoom List"
+    print "==========================================="
+
+    """ Iterate through the ChatRooms list and print the content """
+    for roomName, chatroom in ChatRooms.iteritems():
+        print chatroom
+        print ''
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import re
+
 """
 parsedbContactRemark: input blob buffer for dbContactRemark in WCDB_Contact.sqlite
 format: byte size key + size + value
@@ -33,3 +35,61 @@ def parsedbContactProfile(binBuffer):
         contactProfileArray.append(str(binArray[index:index+size]))
         index += size+1
     return contactProfileArray
+
+"""
+parsedbContactHeadImage: input blob buffer for dbContactHeadImage in WCDB_Contact.sqlite
+format: friend head profile photo miniature & HD one
+output: a tuple of miniPhoto and a HDPhoto
+"""
+def parsedbContactHeadImage(binBuffer):
+    profilePhotoMini = ''
+    profilePhotoHD = ''
+    miniMatchRule = re.compile("http://wx.qlogo.cn/mmhead/([\w]+)/132")
+    miniMatch = miniMatchRule.search(str(binBuffer))
+    if miniMatch:
+        profilePhotoMini = miniMatch.group(0)
+    HDMatchRule = re.compile("http://wx.qlogo.cn/mmhead/([\w]+)/0")
+    HDMatch = HDMatchRule.search(str(binBuffer))
+    if HDMatch:
+        profilePhotoHD = HDMatch.group(0)
+    return (profilePhotoMini, profilePhotoHD)
+
+"""
+parsedbContactChatRoom: input blob buffer for dbContactChatRoom in WCDB_Contact.sqlite
+### There is some issue with the size calculation when the member list got pretty big,
+### the size value is stored with two bytes instead of one in that case, and the values
+### are not corresponding well.
+format: chatroom member userName lists, and a XML payload for RoomData
+output: a tuple of userName Lists, RoomCreator and XML payload data
+"""
+def parsedbContactChatRoom(binBuffer):
+    binArray = bytearray(binBuffer)
+    memberList = []
+    RoomCreator = ''
+    RoomDataXML = ''
+
+    """ Get the member list """
+    memberListSize = binArray[1]
+    memberListBin = binArray[2:2+memberListSize]
+    index = 0
+    memberSize = 0
+
+    while index < memberListSize:
+        if memberListBin[index] == ord(';'):
+            memberList.append(str(memberListBin[index-memberSize:index]))
+            memberSize = 0
+        else:
+            memberSize += 1
+        index += 1
+
+    """ Get the chatroom creator string size/content """
+    RoomCreatorSize = binArray[3+memberListSize]
+    RoomCreator = str(binArray[4+memberListSize:4+memberListSize+RoomCreatorSize])
+
+    """ Get the room xml data """
+    roomDataXMLRule = re.compile("<RoomData>(.*?)<\/RoomData>")
+    roomDataXMLMatch = roomDataXMLRule.search(str(binBuffer))
+    if roomDataXMLMatch:
+        RoomDataXML = roomDataXMLMatch.group(0)
+
+    return (memberList, RoomCreator, RoomDataXML)
